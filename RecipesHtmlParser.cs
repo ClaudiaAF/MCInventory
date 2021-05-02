5e29ef95cccf71d19cfdfe7fa4ffc7b3bd00c667
claud <180181@virtualwindow.co.za>
2021-05-03 00:25:26 +0200

    Recipes parser file created

---
+++ RecipesHtmlParser.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Text;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace MCInventory
+{
+    class RecipesHtmlParser
+    {
+        public static string Process(string input)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(input);
+
+            if (htmlDoc.ParseErrors != null || htmlDoc.DocumentNode == null)
+            {
+                int count = 0;
+                foreach(var htmlParseError in htmlDoc.ParseErrors)
+                {
+                    count ++;
+                    Console.WriteLine("Parse error: " + htmlParseError.Reason);
+                }
+
+                if (count > 0)
+                    throw new FileNotFoundException("corrupt file");
+            }
+
+            return htmlDoc.DocumentNode.InnerHtml;
+
+
+        
+        }
+
+        
+    }
+}
