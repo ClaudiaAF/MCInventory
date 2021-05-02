23724e8958acbc4723ade1da5e06d194f2291b96
claud <180181@virtualwindow.co.za>
2021-05-03 01:02:08 +0200

    resources page item nodes displaying

--- ResourcesHtmlParser.cs
+++ ResourcesHtmlParser.cs
@@ -2,6 +2,7 @@ using System;
 using System.Text;
 using System.IO;
 using HtmlAgilityPack;
+using System.Collections.Generic;
 
 namespace MCInventory
 {
@@ -24,6 +25,18 @@ namespace MCInventory
                 if (count > 0)
                     throw new FileNotFoundException("corrupt file");
             }
+
+            HtmlNode itemNode = htmlDoc.GetElementbyId("itemContainer");
+            Console.WriteLine(itemNode.OuterHtml);
+
+            string[] resourceNames = {"birch", "birch", "birch", "birch", "birch", "birch", "birch", "birch", "birch", "birch", "birch"};
+
+            foreach (string resTitle in resourceNames)
+            {
+                HtmlNode newNode = HtmlNode.CreateNode("<div class='row text-center'><div class='col-sm colm-left'><p value='resource-label' class='resource-label'>Resource Name</p><img src='img/tree-resource.jpg' class='resource-img'/><p value='integer' class='resource-amount' >5</p></div><div class='col-sm colm-middle'><p value='resource-label' class='resource-label'>" + resTitle + "</p><img src='img/tree-resource.jpg' class='resource-img'/><p value='integer' class='resource-amount'>5</p></div><div class='col-sm colm-right'><p value='resource-label' class='resource-label'>Resource Name</p><img src='img/tree-resource.jpg' class='resource-img'/><p value='integer' class='resource-amount'>5</p></div></div>");
+                itemNode.AppendChild(newNode);
+            }
+            
 
             return htmlDoc.DocumentNode.InnerHtml;
 
