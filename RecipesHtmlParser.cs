e96afec7cae4a4692c39e0961615199ee0cf787b
claud <180181@virtualwindow.co.za>
2021-05-03 14:27:01 +0200

    recipes headings displaying dynamically

--- RecipesHtmlParser.cs
+++ RecipesHtmlParser.cs
@@ -25,6 +25,19 @@ namespace MCInventory
                     throw new FileNotFoundException("corrupt file");
             }
 
+            HtmlNode itemNode = htmlDoc.GetElementbyId("recipe-container");
+            Console.WriteLine(itemNode.OuterHtml);
+
+            string[] recipeNames = {"birch", "oak", "coal", "sand", "bucket", "fishing rod", "lapus-lazuli", "cobblestone", "wooden pick-axe", "arrow", "bones"};
+
+            foreach (string recTitle in recipeNames)
+            {
+                HtmlNode newNode = HtmlNode.CreateNode("<div class='row text-center' style='margin-top: 3%;'><div class='col-sm colm-left'><p value='recipe-label' class='recipe-label'>" + recTitle + "</p><img src='img/recipe-grid.jpg' class='recipe-grid-img'/></div><div class='col-sm colm-middle'><p value='recipe-label' class='recipe-label'>" + recTitle + "</p><img src='img/recipe-grid.jpg' class='recipe-grid-img'/></div><div class='col-sm colm-right'><p value='recipe-label' class='recipe-label'>Recipe Name</p><img src='img/recipe-grid.jpg' class='recipe-grid-img'/></div></div>");
+                itemNode.AppendChild(newNode);
+            }
+            
+
+
             return htmlDoc.DocumentNode.InnerHtml;
 
 
