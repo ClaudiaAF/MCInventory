3f7d38833c76132c10d092e200d01c158e47db4d
claud <180181@virtualwindow.co.za>
2021-06-08 01:08:59 +0200

    added recipes layout, fixed items styling

--- parsers/ResourcesHtmlParser.cs
+++ parsers/ResourcesHtmlParser.cs
@@ -36,7 +36,7 @@ namespace MCInventory
              foreach (Block currentBlock in Inventory.items)
             {
                 HtmlNode newNode = HtmlNode.CreateNode(
-                	"<div class='row text-center' style='float:left;'><div class='col-sm colm-left'><div class='card text-center' style='width: 18rem; margin-left:30px; margin-bottom:10%; border-styles'><img class='card-img-top mx-auto' text-align='center' style='width: 150px; height: 150px; padding: 15px;'  src=\""+currentBlock.BlockImage+"\"><div class='card-body'><h5 class='card-title'>" + "<label for=\"" + currentBlock.BlockType + "\">"+currentBlock.BlockType+"</label>" + "</h5><div class='input-group mb-3'><input class='form-control' aria-label='Recipients username' aria-describedby='basic-addon2' type=\"number\" id=\""+currentBlock.BlockType+"\" name=\""+currentBlock.BlockType+"\" value=\""+currentBlock.Count+"\"/><button class='btn btn-outline-success' type='submit' value='Submit'>Add</button></div></div></div></div>");
+                	"<div class='row text-center' style='float:left;'><div class='col-sm colm-left'><div class='card text-center' style='width: 18rem; margin-left:30px; margin-bottom:10%; border-styles'><img class='card-img-top mx-auto' text-align='center' style='width: 150px; height: 150px; padding: 15px;'  src=\""+currentBlock.BlockImage+"\"><div class='card-body'><h5 class='card-title'>" + "<label for=\"" + currentBlock.BlockType + "\">"+currentBlock.BlockType+"</label>" + "</h5><div class='input-group mb-3'><input class='form-control' aria-label='Recipients username' aria-describedby='basic-addon2' type=\"number\" id=\""+currentBlock.BlockType+"\" name=\""+currentBlock.BlockType+"\" value=\""+currentBlock.Count+"\"/><div class='ld-over-full'><button class='btn btn-outline-success ld-over-full' type='submit' value='Submit'>Update</button></div></div></div></div></div>");
                 someNode.AppendChild(newNode);
                 
             }
@@ -45,7 +45,7 @@ namespace MCInventory
 
             foreach(Recipe curRecipe in RecipeBook.Recipes)
             {
-                HtmlNode newNode = HtmlNode.CreateNode("<li class='list-group-item'><input type=\"submit\" value=\""+ curRecipe.Result.BlockType + "\"/></li>");
+                HtmlNode newNode = HtmlNode.CreateNode("<li class='list-group-item'><a href='#' style='border: 0;' class='list-group-item list-group-item-action'><input type=\"submit\" value=\""+ curRecipe.Result.BlockType + "\"/><img src='img/sledgeHammer_Icon.png' class='rounded float-right' style='width:50px; height:50px; margin-top:-49px; margin-right: 12%;'></li></a>");
                 someNode.AppendChild(newNode);
             }
             
